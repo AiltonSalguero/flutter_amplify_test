5939e4fc4bf955d2c35775d2012ccdafff0b032a
Ailton <ailton.salg@gmail.com>
2020-05-04 18:05:59 -0500

    status

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,41 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
+
+FlutterAwsAmplifyCognito.initialize()
+        .then((UserStatus status) {
+          switch (status) {
+            case UserStatus.GUEST:
+              break;
+            case UserStatus.SIGNED_IN:
+              // TODO: Handle this case.
+              break;
+            case UserStatus.SIGNED_OUT:
+              // TODO: Handle this case.
+              break;
+            case UserStatus.SIGNED_OUT_FEDERATED_TOKENS_INVALID:
+              // TODO: Handle this case.
+              break;
+            case UserStatus.SIGNED_OUT_USER_POOLS_TOKENS_INVALID:
+              // TODO: Handle this case.
+              break;
+            case UserStatus.UNKNOWN:
+              // TODO: Handle this case.
+              break;
+            case UserStatus.ERROR:
+              // TODO: Handle this case.
+              break;
+          }
+    }).catchError((error) {
+      print(error);
+    });
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
